5c6990b073adbb63bd8adb7f415b824b2871c9ba
Liviu Ene <eneliviu@gmail.com>
2024-12-24 14:55:35 +0100

    Add routing and navlinks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.css";
+import { Container } from "react-bootstrap";
 import MapLeaflet from "./components/MapLeaflet";
 import Button from "react-bootstrap/Button";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
+        <div className={styles.App}>
             <NavBar />
+            <Container className={styles.Main}>
+                <Routes>
+                    <Route exact path="/" element={<h1>Home page</h1>} />
+                    <Route
+                        exact
+                        path="/signin"
+                        element={<h1>Sign-in page</h1>}
+                    />
+                    <Route
+                        exact
+                        path="/signup"
+                        element={<h1>Sign-up page</h1>}
+                    />
+                    <Route path="*" element={<p>Page not found</p>} />
+                </Routes>
+            </Container>
             <div style={{ paddingTop: "80px" }}>
                 <Button variant="primary">Primary</Button>
             </div>
